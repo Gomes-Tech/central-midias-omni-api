61ef04a2a67eb8892ac5667beb636a0aa971474b
Matheus Martins <matheusmmartins8@gmail.com>
2026-03-20 15:37:31 -0300

    feat: add module health

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Central Midias Omni API')
+    .setDescription('Documentacao da API do projeto Central Midias Omni')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
